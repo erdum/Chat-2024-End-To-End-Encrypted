a9edfb115a358b06aecd2eabecca7925eb5ba5b4
Erdum <erdumadnan@gmail.com>
2023-10-02 16:13:22 +0500

    Add upload & sync user public-key to the server

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,8 @@
 import { onAuthStateChanged } from "firebase/auth";
 import React, { createContext, useEffect, useState } from "react";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import Crypto from "../crypto";
 
 export const AuthContext = createContext();
 
@@ -16,6 +18,24 @@ export const AuthProvider = ({ children }) => {
 
     return () => unsubscribe();
   }, []);
+
+  useEffect(() => {
+    // Sync user public-key with the server
+    (async function () {
+    
+      if (currentUser) {
+        const userDocRef = doc(db, "users", currentUser.uid);
+        const userDocSnap = await getDoc(userDocRef);
+        const exportedPublicKey = await Crypto.exportPublicKey(currentUser.uid);
+
+        if (!userDocSnap.exists() || !userDocSnap.data().publicKey || exportedPublicKey != userDocSnap.data().publicKey) {
+          await updateDoc(userDocRef, {
+            publicKey: exportedPublicKey,
+          });
+        }
+      }
+    })();
+  }, [currentUser]);
 
   return (
     <AuthContext.Provider value={{ currentUser, loading }}>
