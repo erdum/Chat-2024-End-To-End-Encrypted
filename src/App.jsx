6be52ed5d8f979eee266249b94ad4e7a2c40eecf
Erdum <erdumadnan@gmail.com>
2023-10-06 12:02:38 +0500

    Update fetching & syncing users & key-instance on app load

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,81 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "./context/AuthContext";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { 
+  doc,
+  getDocs,
+  getDoc,
+  updateDoc,
+  collection
+} from "firebase/firestore";
+import { db } from "./firebase";
 import icon from "./assets/icon.png";
 import loader from "./assets/puff.svg";
 import Signin from "./Signin";
 import Signup from "./Signup";
 import Home from "./Home";
+import IndexedDB from "./indexedDB";
+import Crypto from "./crypto";
 
 const App = () => {
-  const { currentUser, loading } = useContext(AuthContext);
+  const { currentUser, isUserLoading } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [keyInstance, setKeyInstance] = useState(null);
+
+  useEffect(() => {
+
+    if (isUserLoading) return;
+
+    if (currentUser === null) {
+      (async () => await IndexedDB.clear())();
+      setLoading(false);
+    } else {
+      (async () => {
+        await getUsers();
+        await getOrGenerateKeyInstance();
+        setLoading(false);
+      })();
+    }
+  }, [isUserLoading, currentUser]);
+
+  const getUsers = async () => {
+    const usersCollection = collection(db, "users");
+    const usersSnapshot = await getDocs(usersCollection);
+    const usersData = usersSnapshot.docs.map((doc) => doc.data());
+    setUsers(usersData);
+  };
+
+  const getOrGenerateKeyInstance = async () => {
+    const localKeyInstance = await IndexedDB.getKeyInstance(currentUser.uid);
+
+    if (localKeyInstance === null) {
+      const newKeyInstance = await Crypto.generateKeyPairInstance();
+      const exportedPublicKey = await Crypto.exportPublicKey(
+        newKeyInstance.publicKey
+      );
+      await IndexedDB.saveKeyInstance(newKeyInstance, currentUser.uid);
+      
+      const userDocRef = doc(db, "users", currentUser.uid);
+      await updateDoc(userDocRef, {
+        publicKey: exportedPublicKey
+      });
+      
+      setKeyInstance(newKeyInstance);
+    } else {
+      const exportedPublicKey = await Crypto.exportPublicKey(
+        localKeyInstance.publicKey
+      );
+
+      const userDocRef = doc(db, "users", currentUser.uid);
+      await updateDoc(userDocRef, {
+        publicKey: exportedPublicKey
+      });
+      
+      setKeyInstance(localKeyInstance);
+    }
+  };
+
   const ProtectedRoute = ({ children }) => {
     if (!currentUser) {
       return <Navigate to="/signin" />;
@@ -34,7 +101,7 @@ const App = () => {
         path="/"
         element={
           <ProtectedRoute>
-            <Home />
+            <Home users={users} />
           </ProtectedRoute>
         }
       />
