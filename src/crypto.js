937c6f8940ab2e7f6a1630ee3c50da49a53a0aad
Erdum <erdumadnan@gmail.com>
2023-10-10 11:00:30 +0500

    Refactor indentation corrected for consistency

--- src/crypto.js
+++ src/crypto.js
@@ -1,56 +1,56 @@
 const Crypto = (() => {
-	// publicKey: CryptoKey, returns exported public key in jwk format
-	const exportPublicKey = async (publicKey) => {
-		const exportPublicKey = await window.crypto.subtle.exportKey('jwk', publicKey);
-		return exportPublicKey;
-	};
+  // publicKey: CryptoKey, returns exported public key in jwk format
+  const exportPublicKey = async (publicKey) => {
+    const exportPublicKey = await window.crypto.subtle.exportKey('jwk', publicKey);
+    return exportPublicKey;
+  };
 
-	// return CryptoKey object with asymmetric keyPair
-	const generateKeyPairInstance = async () => {
-		const newKeyPairInstance = await window.crypto.subtle.generateKey(
-			{
-		    name: "RSA-OAEP",
-		    modulusLength: 4096,
-		    publicExponent: new Uint8Array([1, 0, 1]),
-		    hash: "SHA-256"
-		  },
-		  false,
-		  ["decrypt", "encrypt"]
-		);
-		return newKeyPairInstance;
-	};
+  // return CryptoKey object with asymmetric keyPair
+  const generateKeyPairInstance = async () => {
+    const newKeyPairInstance = await window.crypto.subtle.generateKey(
+      {
+        name: "RSA-OAEP",
+        modulusLength: 4096,
+        publicExponent: new Uint8Array([1, 0, 1]),
+        hash: "SHA-256"
+      },
+      false,
+      ["decrypt", "encrypt"]
+    );
+    return newKeyPairInstance;
+  };
 
-	// data: string, receiverPublicKey: asymmetric public-key in (jwk) format
-	// return: base64
-	const encodeCipher = async (data, receiverPublicKey) => {
-		const key = await window.crypto.subtle.importKey('jwk', receiverPublicKey, {
-	    name: "RSA-OAEP",
-	    hash: "SHA-256"
-	  }, true, ['encrypt']);
-		const cipherBytesArray = await window.crypto.subtle.encrypt({
-			name: "RSA-OAEP"
-		}, key, new TextEncoder().encode(data));
-		return arrayBufferToBase64(cipherBytesArray);
-	};
+  // data: string, receiverPublicKey: asymmetric public-key in (jwk) format
+  // return: base64
+  const encodeCipher = async (data, receiverPublicKey) => {
+    const key = await window.crypto.subtle.importKey('jwk', receiverPublicKey, {
+      name: "RSA-OAEP",
+      hash: "SHA-256"
+    }, true, ['encrypt']);
+    const cipherBytesArray = await window.crypto.subtle.encrypt({
+      name: "RSA-OAEP"
+    }, key, new TextEncoder().encode(data));
+    return arrayBufferToBase64(cipherBytesArray);
+  };
 
-	// cipher: base64
-	// return: string
-	const decodeCipher = async (cipher, receiverPrivateKey) => {
-		const decodedCipher = await window.crypto.subtle.decrypt({
-			name: "RSA-OAEP"
-		}, receiverPrivateKey, base64ToArrayBuffer(cipher));
-		return new TextDecoder().decode(decodedCipher);
-	};
+  // cipher: base64
+  // return: string
+  const decodeCipher = async (cipher, receiverPrivateKey) => {
+    const decodedCipher = await window.crypto.subtle.decrypt({
+      name: "RSA-OAEP"
+    }, receiverPrivateKey, base64ToArrayBuffer(cipher));
+    return new TextDecoder().decode(decodedCipher);
+  };
 
-	// ciphers: array[base64]
-	// return: array[string]
-	const decodeAllCiphers = async (ciphers, receiverPrivateKey) => {
-		const promises = [];
-		ciphers.forEach(cipher => promises.push(decodeCipher(cipher, receiverPrivateKey)));
-		return Promise.all(promises);
-	};
+  // ciphers: array[base64]
+  // return: array[string]
+  const decodeAllCiphers = async (ciphers, receiverPrivateKey) => {
+    const promises = [];
+    ciphers.forEach(cipher => promises.push(decodeCipher(cipher, receiverPrivateKey)));
+    return Promise.all(promises);
+  };
 
-	const arrayBufferToBase64 = (buffer) => {
+  const arrayBufferToBase64 = (buffer) => {
     var binary = '';
     var bytes = new Uint8Array(buffer);
     var len = bytes.byteLength;
@@ -68,15 +68,15 @@ const Crypto = (() => {
       bytes[i] = binary_string.charCodeAt(i);
     }
     return bytes.buffer;
-	}
+  }
 
-	return {
-		generateKeyPairInstance,
-		exportPublicKey,
-		encodeCipher,
-		decodeCipher,
-		decodeAllCiphers,
-	};
+  return {
+    generateKeyPairInstance,
+    exportPublicKey,
+    encodeCipher,
+    decodeCipher,
+    decodeAllCiphers,
+  };
 })();
 
 export default Crypto;
--- src/indexedDB.js
+++ src/indexedDB.js
@@ -1,88 +1,88 @@
 const IndexedDB = (() => {
-	const initialize = async () => {
-		const request = window.indexedDB.open('dchat-store', 1);
+  const initialize = async () => {
+    const request = window.indexedDB.open('dchat-store', 1);
 
-		request.onupgradeneeded = ({ target: { result: db }}) => {
-			db.createObjectStore('cryptoInstance');
-			db.createObjectStore('messages');
-		};
-		return new Promise((resolve, reject) => {
-			request.onerror = (event) => console.error(event);
-			request.onsuccess = () => resolve(request.result);
-		});
-	};
+    request.onupgradeneeded = ({ target: { result: db }}) => {
+      db.createObjectStore('cryptoInstance');
+      db.createObjectStore('messages');
+    };
+    return new Promise((resolve, reject) => {
+      request.onerror = (event) => console.error(event);
+      request.onsuccess = () => resolve(request.result);
+    });
+  };
 
-	const getKeyInstance = async (uid) => {
-		const db = await initialize();
-		const transaction = db.transaction('cryptoInstance', 'readonly');
-		const cryptoInstanceStore = transaction.objectStore('cryptoInstance');
-		const request = cryptoInstanceStore.get(uid);
-		db.close();
-		return new Promise((resolve, reject) => {
-			request.onsuccess = () => resolve(request.result ?? null);
-		});
-	};
+  const getKeyInstance = async (uid) => {
+    const db = await initialize();
+    const transaction = db.transaction('cryptoInstance', 'readonly');
+    const cryptoInstanceStore = transaction.objectStore('cryptoInstance');
+    const request = cryptoInstanceStore.get(uid);
+    db.close();
+    return new Promise((resolve, reject) => {
+      request.onsuccess = () => resolve(request.result ?? null);
+    });
+  };
 
-	const saveKeyInstance = async (object, uid) => {
-		const db = await initialize();
-		const transaction = db.transaction('cryptoInstance', 'readwrite');
-		const cryptoInstanceStore = transaction.objectStore('cryptoInstance');
-		const request = cryptoInstanceStore.put(object, uid);
-		db.close();
-		return new Promise((resolve, reject) => {
-			request.onsuccess = () => resolve(request);
-		});
-	};
+  const saveKeyInstance = async (object, uid) => {
+    const db = await initialize();
+    const transaction = db.transaction('cryptoInstance', 'readwrite');
+    const cryptoInstanceStore = transaction.objectStore('cryptoInstance');
+    const request = cryptoInstanceStore.put(object, uid);
+    db.close();
+    return new Promise((resolve, reject) => {
+      request.onsuccess = () => resolve(request);
+    });
+  };
 
-	const clearInstances = async (db) => {
-		const transaction = db.transaction('cryptoInstance', 'readwrite');
-		const cryptoInstanceStore = transaction.objectStore('cryptoInstance');
-		cryptoInstanceStore.clear();
-	};
+  const clearInstances = async (db) => {
+    const transaction = db.transaction('cryptoInstance', 'readwrite');
+    const cryptoInstanceStore = transaction.objectStore('cryptoInstance');
+    cryptoInstanceStore.clear();
+  };
 
-	const saveMessages = async (messages, uid) => {
-		const db = await initialize();
-		const transaction = db.transaction('messages', 'readwrite');
-		const messagesStore = transaction.objectStore('messages');
-		const request = messagesStore.put(messages, uid);
-		db.close();
-		return new Promise((resolve, reject) => {
-			request.onsuccess = () => resolve(request);
-		});
-	};
+  const saveMessages = async (messages, uid) => {
+    const db = await initialize();
+    const transaction = db.transaction('messages', 'readwrite');
+    const messagesStore = transaction.objectStore('messages');
+    const request = messagesStore.put(messages, uid);
+    db.close();
+    return new Promise((resolve, reject) => {
+      request.onsuccess = () => resolve(request);
+    });
+  };
 
-	const getMessages = async (uid) => {
-		const db = await initialize();
-		const transaction = db.transaction('messages', 'readonly');
-		const messagesStore = transaction.objectStore('messages');
-		const request = messagesStore.get(uid);
-		db.close();
-		return new Promise((resolve, reject) => {
-			request.onsuccess = () => resolve(request.result ?? []);
-		});
-	};
+  const getMessages = async (uid) => {
+    const db = await initialize();
+    const transaction = db.transaction('messages', 'readonly');
+    const messagesStore = transaction.objectStore('messages');
+    const request = messagesStore.get(uid);
+    db.close();
+    return new Promise((resolve, reject) => {
+      request.onsuccess = () => resolve(request.result ?? []);
+    });
+  };
 
-	const clearMessages = async (db) => {
-		const transaction = db.transaction('messages', 'readwrite');
-		const messagesStore = transaction.objectStore('messages');
-		messagesStore.clear();
-	};
+  const clearMessages = async (db) => {
+    const transaction = db.transaction('messages', 'readwrite');
+    const messagesStore = transaction.objectStore('messages');
+    messagesStore.clear();
+  };
 
-	const clear = async () => {
-		const db = await initialize();
-		clearInstances(db);
-		clearMessages(db);
-		db.close();
-	};
+  const clear = async () => {
+    const db = await initialize();
+    clearInstances(db);
+    clearMessages(db);
+    db.close();
+  };
 
-	return {
-		initialize,
-		getKeyInstance,
-		saveKeyInstance,
-		saveMessages,
-		getMessages,
-		clear,
-	}
+  return {
+    initialize,
+    getKeyInstance,
+    saveKeyInstance,
+    saveMessages,
+    getMessages,
+    clear,
+  }
 })();
 
 export default IndexedDB;
