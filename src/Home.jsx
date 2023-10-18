57a6ae439435617edefbda178fb41fd34e90c970
Erdum <erdumadnan@gmail.com>
2023-10-18 23:09:21 +0500

    Update swipable mobile sidebar

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,10 +4,48 @@ import Sidebar from "./Sidebar";
 
 const Home = () => {
   const [selectedUser, setSelectedUser] = useState(null);
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+  const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(true);
+
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null);
+    setTouchStart(e.nativeEvent.touches[0].pageX);
+  }
+
+  const onTouchMove = (e) => {
+    setTouchEnd(e.nativeEvent.touches[0].pageX);
+  }
+
+  const onTouchEnd = () => {
+    
+    if (!touchStart || !touchEnd) return;
+    
+    if (touchEnd - touchStart > minSwipeDistance) {
+      setIsMobileSidebarOpen(true);
+    }
+
+    if (touchStart - touchEnd > minSwipeDistance) {
+      setIsMobileSidebarOpen(false);
+    }
+  }
 
   return (
-    <div className="flex min-h-screen bg-slate-100 w-full lg:max-w-[1400px] lg:min-w-[850px] mx-auto">
-      <div className="bg-white hidden lg:block lg:w-[400px] w-[300px]">
+    <div
+      className="flex min-h-screen bg-slate-100 w-full lg:max-w-[1400px] lg:min-w-[850px] mx-auto"
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
+    >
+      <div className="bg-white hidden lg:block lg:w-[400px]">
+        <Sidebar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+      </div>
+      <div className={`absolute top-0 left-0 h-full bg-white z-10 transition-all overflow-x-hidden ${isMobileSidebarOpen ? 'w-5/6' : 'w-0'} lg:hidden`}>
         <Sidebar
           selectedUser={selectedUser}
           setSelectedUser={setSelectedUser}
