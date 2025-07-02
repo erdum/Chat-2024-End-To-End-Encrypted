4ac8113da214f573081e2366724e92ec6551de69
Erdum <erdumadnan@gmail.com>
2025-07-03 00:15:48 +0500

    Create store.js

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { create } from 'zustand'
+
+const useStore = create((set) => ({
+  selectedUser: null,
+  isSidebarOpen: false,
+  messages: [],
+
+  setSidebarOpen: () => set((state) => ({ ...state, isSidebarOpen: true })),
+  setSidebarClose: () => set((state) => ({ ...state, isSidebarOpen: false })),
+
+  setSelectedUser: (user) => set((state) => ({ ...state, selectedUser: user })),
+
+  setMessages: (messages) => set((state) => ({ ...state, messages })),
+}));
